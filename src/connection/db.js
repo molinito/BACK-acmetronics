a5cd6ab79f062eebb5d3f9c0ba85c825cb303c38
Marcelo Saravia <molinito48@gmail.com>
2023-01-12 10:13:18 -0300

    first commit

---
+++ src/connection/db.js
@@ -0,0 +1,78 @@
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, "../models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "../models", file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+
+const { User, Product, Category, Gmailuser, Order, OrderUser, Comment } =
+
+  sequelize.models;
+
+// Aca vendrian las relaciones
+// Product.hasMany(Reviews);
+Category.belongsToMany(Product, {
+  through: "Category_Product",
+  as: "CategoryProduct",
+});
+Product.belongsToMany(Category, {
+  through: "Category_Product",
+  as: "CategoryProduct",
+});
+
+Order.belongsToMany(Gmailuser, {through: "Gmailuser_Order", as: "GmailuserOrder"})
+Gmailuser.belongsToMany(Order, {through: "Gmailuser_Order", as: "GmailuserOrder"})
+
+User.belongsToMany(Product, { through: "User_Favorite", as: "Favorites" });
+Product.belongsToMany(User, { through: "User_Favorite", as: "Favorites" });
+
+Gmailuser.belongsToMany(Product, {
+  through: "Gmailuser_Favorite",
+  as: "Gmailfavs",
+});
+Product.belongsToMany(Gmailuser, {
+  through: "Gmailuser_Favorite",
+  as: "Gmailfavs",
+});
+Product.belongsToMany(Comment, {
+  through: "Product_comments",});
+
+  Comment.belongsToMany(Product, {
+  through: "Product_comments",});
+
+module.exports = {
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+};
